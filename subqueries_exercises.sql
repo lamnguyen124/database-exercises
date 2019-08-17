a2b5402c588c590a1e242dda94d52bdfed2e45cf
Lam Nguyen <nguyen.h.lam12@gmail.com>
2019-08-17 14:43:09 -0500

    subqueries

---
+++ subqueries_exercises.sql
@@ -0,0 +1,18 @@
+use employees;
+
+select first_name from employees
+    where hire_date IN (
+        select hire_date from employees
+        where emp_no = '101010');
+
+select title from titles
+      where emp_no IN (
+          select emp_no from employees
+          where first_name = 'Aamod');
+
+select first_name, last_name from employees
+    where emp_no IN (
+        select emp_no from dept_manager
+         where emp_no IN(
+            select  emp_no from employees
+            where gender = 'f' ));
