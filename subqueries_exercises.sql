ce318275e227bbe8fdbd8f6b9b9b3333095fbefa
Lam Nguyen <nguyen.h.lam12@gmail.com>
2019-08-17 15:02:25 -0500

    finished

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -16,3 +16,21 @@ select first_name, last_name from employees
          where emp_no IN(
             select  emp_no from employees
             where gender = 'f' ));
+
+select dept_name from departments
+    where dept_no IN(
+        select dept_no from dept_manager
+            where  to_date > now()
+            and emp_no IN (
+                select emp_no from employees
+                    where gender = 'f'
+                )
+        );
+
+select first_name, last_name from employees
+    where emp_no IN(
+        select emp_no from salaries
+            where salary IN(
+                select max(salary) from salaries
+                )
+        );
